65e58571eb900f81cdcf5f114810b8998c9b9f2f
Prince <princev2451@gmail.com>
2022-10-19 23:23:31 +0530

    ➕ mongoose added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import "./dotenv";
 import express from "express";
 import cors from "cors";
+import { connect } from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT, () => {
-  console.log("Server started at", process.env.PORT);
-});
+connect(process.env.MONGO_DB_URL_STRING)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server started at", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
