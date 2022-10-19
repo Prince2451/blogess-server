ea639322c453fa7c4c9649856b70e6cf57e739da
Prince <princev2451@gmail.com>
2022-10-19 23:01:30 +0530

    🥅 errors fixed

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 
-function setConfig() {
+function setConfig(): void {
   config({
     path: process.cwd() + "/.env." + (process.env.NODE_ENV || "development"),
   });
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server started at", process.env.PORT);
+});
