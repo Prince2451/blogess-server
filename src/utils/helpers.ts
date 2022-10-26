e611b1a52a7f0d55494325c78dbf0bb0877f7f7a
Prince <princev2451@gmail.com>
2022-10-26 12:30:35 +0530

    🎨 converted to asynd function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,14 +17,14 @@ class HTTPError<T extends object = object> extends Error {
 function createRequestHandler(
   fn: (...agrs: Parameters<RequestHandler>) => Promise<void> | void
 ): (...args: Parameters<RequestHandler>) => void {
-  return function (...args: Parameters<RequestHandler>) {
-    // will not be able to handle errors inside .then or .catch in promise chain
-    const returnedValue = fn(...args);
-    if (returnedValue instanceof Promise) {
-      returnedValue.catch((err) => {
-        // Goes to default error handlers once error occurs inside requestHandler
-        args[2](err);
-      });
+  return async function (...args: Parameters<RequestHandler>) {
+    try {
+      // will not be able to handle errors inside .then or .catch in promise chain
+      // only returned promises will be handled
+      await fn(...args);
+    } catch (err) {
+      // Goes to default error handlers once error occurs inside requestHandler
+      args[2](err);
     }
   };
 }
