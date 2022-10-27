4dca2865d03d8a1f626eea51c93e540fda4fa38b
Prince <princev2451@gmail.com>
2022-10-27 11:28:26 +0530

    👔 user static methods added

--- src/models/auth/user.ts
+++ src/models/auth/user.ts
@@ -1,7 +1,20 @@
-import { model, Schema } from "mongoose";
+import { pbkdf2, randomBytes } from "crypto";
+import { Model, model, Schema } from "mongoose";
 import { auth } from "../../typings";
 
-const userSchema = new Schema<auth.User>({
+interface UserStatics {
+  hashPassword: (password: string) => Promise<string>;
+  verifyPassword: (password: string, hasedPassword: string) => Promise<boolean>;
+}
+
+const userSchema = new Schema<
+  auth.User,
+  Model<auth.User>,
+  {},
+  {},
+  {},
+  UserStatics
+>({
   firstName: {
     type: String,
     required: true,
@@ -28,6 +41,30 @@ const userSchema = new Schema<auth.User>({
     required: true,
   },
 });
+
+userSchema.statics.hashPassword = async function (password) {
+  const salt = randomBytes(16).toString("hex");
+  return await new Promise((resolve, reject) => {
+    pbkdf2(password, salt, 1000, 64, "sha512", (err, hash) => {
+      if (err) reject(err);
+      else {
+        resolve(hash.toString("hex") + "." + salt);
+      }
+    });
+  });
+};
+
+userSchema.statics.verifyPassword = async function (password, hasedPassword) {
+  const [storedHash, salt] = hasedPassword.split(".");
+  return await new Promise((resolve, reject) => {
+    pbkdf2(password, salt, 1000, 64, "sha512", (err, hash) => {
+      if (err) reject(err);
+      else {
+        resolve(storedHash === hash.toString("hex"));
+      }
+    });
+  });
+};
 
 const User = model("User", userSchema);
 
