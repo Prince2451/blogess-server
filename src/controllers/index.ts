a0b1f2dc8e5332f14b86ffc941d84fe4cad7f5ec
Prince <princev2451@gmail.com>
2022-11-24 23:59:45 +0530

    👔 posts controller exported

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,8 @@
 import authValidators from "./auth/validators";
+import postsValidators from "./posts/validators";
 export * as auth from "./auth";
+export * as posts from "./posts";
 
-const validators = { auth: authValidators };
+const validators = { auth: authValidators, posts: postsValidators };
 
 export { validators };
